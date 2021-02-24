9d70bf964a5e5c92b5b309a94714498a8c74c86a
raghunathanbakkianathan <52690170+raghunathanbakkianathan@users.noreply.github.com>
2021-02-24 13:00:56 +0530

    Update analyser.sh

--- analyser.sh
+++ analyser.sh
@@ -4,7 +4,7 @@ SONAR_TOKEN="{YOUR-TOKEN}"
 PROJECT_NAME="$( echo "$1" | sed 's@.*/@@' )"
 cd src/
 sonar-scanner \
-  -Dsonar.projectKey=SalesTransactions \
+  -Dsonar.projectKey=SalesTransactions1 \
   -Dsonar.sources=. \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=c203d789a28b4f201ebbe09ccfa8d6b0f7fabde9
+  -Dsonar.host.url=http://172.20.10.2:9000 \
+  -Dsonar.login=ed2cccfd4c6e9aaffd66d3fe49e7ba22f47ebe8a
